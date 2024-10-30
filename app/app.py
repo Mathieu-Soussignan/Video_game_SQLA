f427627bd82116a36a6526c6d68aada2b6fc2227
Mathieu Soussignan <contact.mathieu.soussignan@gmail.com>
2024-10-30 15:28:51 +0100

    Update fichier app.py et rédaction du fichier utils.py

--- app/app.py
+++ app/app.py
@@ -11,8 +11,8 @@ session = create_session()
 
 def load_data():
     data = session.query(Ventes).all()
-    df = pd.DataFrame()
-
+    df = pd.DataFrame([(d.id_ventes, d.id_jeu, d.na_sales, d.eu_sales, d.jp_sales, d.other_sales, d.global_sales) for d in data],
+                      columns=['ID_Ventes', 'ID_Jeu', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales'])
     return df
 
 df = load_data()
@@ -24,10 +24,10 @@ st.header("Explorer les données")
 st.write(df.head())
 
 st.header("Ventes globales par région")
-sales_by_region = df[[]].sum()
+sales_by_region = df[['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']].sum()
 st.bar_chart(sales_by_region)
 
 st.header('Distribution des ventes par région')
 fig, ax = plt.subplots()
-sns.boxplot(data=df[[]], ax=ax)
+sns.boxplot(data=df[['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']], ax=ax)
 st.pyplot(fig)
